c823d7b90acdedc8c28ec438d8f352eac91597ad
PIYUSH KUMAR <piyush.k9013@gmail.com>
2015-09-14 02:58:40 +0530

    changes in the reducer of new api

--- src/main/java/com/practice/hadoop/newapi/NewWordReducer.java
+++ src/main/java/com/practice/hadoop/newapi/NewWordReducer.java
@@ -5,14 +5,16 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
-public class NewWordReducer extends Reducer {
+public class NewWordReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
 
-    public void reduce(Text key, Iterable<IntWritable> values, Reducer.Context context) throws IOException, InterruptedException {
+    public void reduce(Text key, Iterable<IntWritable> values, Reducer<Text,IntWritable,Text,IntWritable>.Context context) throws IOException, InterruptedException {
         int count = 0;
 
-        while (values.iterator().hasNext()) {
-            count += values.iterator().next().get();
+        Iterator<IntWritable> iterator = values.iterator();
+        while (iterator.hasNext()) {
+            count += iterator.next().get();
         }
         context.write(key, new IntWritable(count));
     }
